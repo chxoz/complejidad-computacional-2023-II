a479c83c932dc22d18eca967a5703087577503d4
Lopez Miranda Angel Mauricio <mauriciolmiranda@ciencias.unam.mx>
2023-02-23 14:11:40 -0600

    codificacion terminada

--- practica1/programa/set_cover.py
+++ practica1/programa/set_cover.py
@@ -23,10 +23,14 @@ else:
             print("los primeros 5 y los ultimos 5 elementos de S son :" + str(first) + str(last))
         else:
             print("elementos del conjunto S: "+ str(list_s))
-            
+    if c in large_string:
+        str_c = str(lines[2]).replace('\\',"").replace("\n", "")
+        list_c = str_c.split("%")
+        l_c = len(list_c)
+        print("el numero de miembros de c es: " + str(l_c))
         
     if k in large_string:
-        print("Valor de K = " + str(lines[0].replace('\\' ,"")))
+        print("Valor de K = " + str(lines[0].replace('#' ,"")))
     else:
         print("no se encontro el valor de K en la codificacion")
   
